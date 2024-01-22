fe04108f0ec7db6cd3682d742917990fca759b55
Max <pinkymaxou08@gmail.com>
2024-01-21 21:08:38 -0500

    Add int32 version of the circle calculation

--- misc-formula/misc-formula.c
+++ misc-formula/misc-formula.c
@@ -6,6 +6,12 @@ double MISCFA_CircleDiff(double a, double b, double rotation)
     return fmod((a - b + rotation + halfRotation), rotation) - halfRotation;
 }
 
+int32_t MISCFA_CircleDiffd32(int32_t a, int32_t b, int32_t rotation)
+{
+    const int32_t halfRotation = rotation / 2;
+    return fmod((a - b + rotation + halfRotation), rotation) - halfRotation;
+}
+
 double MISCFA_LinearizeLEDOutput(double dOfOne)
 {
     return (pow(10.0d, dOfOne) - 1.0d) / 9.0d;
--- misc-formula/misc-formula.h
+++ misc-formula/misc-formula.h
@@ -9,6 +9,8 @@ extern "C" {
 
 double MISCFA_CircleDiff(double a, double b, double rotation);
 
+int32_t MISCFA_CircleDiffd32(int32_t a, int32_t b, int32_t rotation);
+
 double MISCFA_LinearizeLEDOutput(double dOfOne);
 
 #ifdef __cplusplus
